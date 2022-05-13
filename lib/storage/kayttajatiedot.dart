d0612f3cf4c809fc8213f2725e28ad1dca972b3d
Juuso-Petteri Kuisma <juuso.kuisma@gmail.com>
2022-05-13 13:11:33 +0300

    Lisaa Kayttajatiedot-luokka

--- lib/storage/kayttajatiedot.dart
+++ lib/storage/kayttajatiedot.dart
@@ -1,1 +1,11 @@
-// Toteuta tänne luokka Kayttajatiedot
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Kayttajatiedot {
+  tuttuKayttaja() async {
+    SharedPreferences asetukset = await SharedPreferences.getInstance();
+    var tuttuKayttaja = asetukset.constantKey('TUTTU_KAYTTAJA');
+    asetukset.setString('TUTTU_KAYTTAJA', 'KYLLA');
+
+    return tuttuKayttaja;
+  }
+}
