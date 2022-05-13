52c8fdd1b41f818ab5df3f00375d8874289831f7
Juuso-Petteri Kuisma <juuso.kuisma@gmail.com>
2022-05-13 13:05:55 +0300

    Add SafeAreas

--- lib/views/aloitusnakyma.dart
+++ lib/views/aloitusnakyma.dart
@@ -1,14 +1,12 @@
 import 'package:flutter/material.dart';
 
 class Aloitusnakyma extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     final nappi = ElevatedButton(
-      child: Text('Kysy kysymys'),
-      onPressed: () => Navigator.pushNamed(context, '/kysely')
-    );
+        child: Text('Kysy kysymys'),
+        onPressed: () => Navigator.pushNamed(context, '/kysely'));
 
-    return Scaffold(body: nappi);
+    return Scaffold(body: SafeArea(child: nappi));
   }
 }
--- lib/views/kyselynakyma.dart
+++ lib/views/kyselynakyma.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 
 class Kyselynakyma extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Text('Hei maailma!'));
+    return Scaffold(body: SafeArea(child: Text('Hei maailma!')));
   }
 }
